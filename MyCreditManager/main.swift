ee030161c04683a818f7a944e6eef43e68f1208d
derrickkim0109 <derrickkim0109@naver.com>
2022-11-13 19:44:36 +0900

    feat: MyCreditManager 프로젝트 생성

---
+++ MyCreditManager/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  MyCreditManager
+//
+//  Created by Derrick kim on 2022/11/13.
+//
+
+import Foundation
+
+func run() {
+    print("원하는 기능을 입력해주세요.")
+    let inputValue = readLine()
+
+
+}
+
+enum func
